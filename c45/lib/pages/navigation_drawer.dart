e3c83975cb139234b61bdf54e0db4e8d3b569cd8
srlopez <you@example.com>
2020-11-22 20:30:37 +0100

    fin por ahora

--- c45/lib/pages/navigation_drawer.dart
+++ c45/lib/pages/navigation_drawer.dart
@@ -100,8 +100,8 @@ class MiDrawerBodyItem extends StatelessWidget {
           Icon(icon),
           Padding(
             padding: EdgeInsets.only(left: 8.0),
-            child:
-                Text(text, style: GoogleFonts.balooTamma(textStyle: display1)),
+            child: Text(text,
+                style: GoogleFonts.sourceSansPro(textStyle: display1)),
           )
         ],
       ),
