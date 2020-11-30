e5cd80ce85d2658fce4b011a4dd7c51f8712d3c5
srlopez <you@example.com>
2020-11-30 10:25:45 +0100

    eliminado backup

--- c45cpy/android/app/src/main/kotlin/com/example/c45/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.c45
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- c45cpy/lib/routes/pageRoute.dart
+++
@@ -1,9 +0,0 @@
-library routes;
-
-import '../pages/candidatos_page.dart';
-
-export '../pages/candidatos_page.dart';
-
-class pageRoutes {
-  static const String candidatos = CandidatosPage.routeName;
-}
